3c6e89b306dadfd2306b6cb503e04d2ffc27f1d0
Tyler Suzuki Nelson <tyler.nelson@alumni.ubc.ca>
2015-03-09 12:05:26 -0700

    Added users model

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150309185801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "twitter_consumer_token"
+    t.string   "twitter_consumer_secret"
+    t.text     "omniauth_raw_data"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
 
 end
