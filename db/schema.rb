bbf19d7e1fbb18268b1a715128df109dbd4a3635
Brandyn Prasad <brandyn.prasad@gmail.com>
2015-03-09 16:32:18 -0700

    can now see facebook login when visiting localhost:3000/auth/facebook

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150309201826) do
+ActiveRecord::Schema.define(version: 20150309232723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,8 @@ ActiveRecord::Schema.define(version: 20150309201826) do
     t.text     "omniauth_raw_data"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+    t.string   "token"
+    t.datetime "expires_at"
   end
 
   add_foreign_key "boards", "users", column: "player1"
