2f9bae768abaf3ee395d6b4705b96d7ee3b35a66
codeBirdie123 <mskinkade@gmail.com>
2015-03-10 16:56:27 -0700

    implemented piece checking for queen ne and se moves

--- app/controllers/game_pieces_controller.rb
+++ app/controllers/game_pieces_controller.rb
@@ -576,9 +576,11 @@ private
   def get_queen_ne_moves(row, column)
     output = []
     while row >= 1 && column.ord <= 104
+      column = (column.ord + 1).chr
+      row -= 1      
       board_square = BoardSquare.where("row = ?", row).find_by_column column
 
-      if board_square.game_piece
+      if board_square && board_square.game_piece
         if board_square.game_piece.user != current_user
           # encountered opponent's game piece - add space to possible move
           output << [row, column] if is_valid_position(row, column)      
@@ -586,8 +588,6 @@ private
         break
       else
         output << [row, column] if is_valid_position(row, column)      
-        column = (column.ord + 1).chr
-        row -= 1      
       end
     end  
     output.compact
@@ -597,18 +597,18 @@ private
   def get_queen_se_moves(row, column)
     output = []
     while row <= 8 && column.ord <= 104
+      column = (column.ord + 1).chr
+      row += 1 
       board_square = BoardSquare.where("row = ?", row).find_by_column column
 
-      if board_square.game_piece
+      if board_square && board_square.game_piece
         if board_square.game_piece.user != current_user
           # encountered opponent's game piece - add space to possible move
           output << [row, column] if is_valid_position(row, column)      
         end
         break
       else
-        output << [row, column] if is_valid_position(row, column)      
-        column = (column.ord + 1).chr
-        row += 1    
+        output << [row, column] if is_valid_position(row, column)         
       end
     end  
     output.compact
